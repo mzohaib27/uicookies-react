0f52061e133e24769a6b179ea001e4688bcdfe2e
mzohaib27 <mzohaibhasan27@gmail.com>
2024-07-03 21:39:24 +0400

    uicookies-react

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -21,6 +21,12 @@ const persistedReducer = persistReducer(
 
 const store = configureStore({
   reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
 const persistor = persistStore(store);
