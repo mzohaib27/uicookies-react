1196ea16040c52aed5e3a2a5f599839a638117d2
mzohaib27 <129533019+mzohaib27@users.noreply.github.com>
2024-07-04 10:57:51 +0400

    Update Footer.jsx
    updating

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { styles } from "../utils/Styles";
-import Button from "./Button";
+import { styles } from "../utils/Styles.js";
+import Button from "./Button.jsx";
 
 const Footer = () => {
   return (
